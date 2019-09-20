680576fdcd7e5a6fef0999abea5ff113953731cf
Felipe Silva <felipesilva@Felipes-MacBook-Pro.local>
2019-09-19 23:40:56 -0300

    Finished all the challenges from the third module using webpack

---
+++ desafio2/src/main.js
@@ -0,0 +1,17 @@
+//1.1
+import ClasseUsuario from "./functions";
+
+ClasseUsuario.info();
+
+//1.2
+import { idade } from "./functions";
+
+console.log(idade);
+
+//1.3
+
+import ClasseUsuario, { idade as IdadeUsuario } from "./functions";
+
+ClasseUsuario.info();
+
+console.log(IdadeUsuario);
